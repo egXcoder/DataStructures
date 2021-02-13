c28b1654268d0539c7fb1bc37180872924f5f22d
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-02-13 17:02:41 +0200

    set a test for linkedlist

--- src/com/company/ds/LinkedList.java
+++ src/com/company/ds/LinkedList.java
@@ -58,4 +58,127 @@ public class LinkedList<E> {
         tail=node;
         currentSize++;
     }
+
+    /**
+     * Removing from the beginning of linkedlist, O(1) operation
+     * @return
+     */
+    public E removeFirst(){
+        //empty linkedlist
+        if(head==null){
+            return null;
+        }
+
+        E tmp = head.data;
+
+        if(head==tail){
+            //only single Element exist
+            head = tail = null;
+        }else {
+            //multiple elements exist
+            head = head.next;
+        }
+
+        currentSize--;
+        return tmp;
+    }
+
+    /**
+     * remove From The end of linkedlist, O(N) operation as this linkedlist is implementation of single L.L
+     * Doubley L.L will have in each node, next, prev pointers which can take more memory but its going to improve this operation
+     * @return
+     */
+    public E removeLast(){
+        //empty linkedlist
+        if(head==null){
+            return null;
+        }
+
+        //single linkedlist
+        if(head == tail){
+            E lastData = head.data;
+            head=tail=null;
+            currentSize--;
+            return lastData;
+        }
+
+        Node<E> tmp1 = head;
+        Node<E> tmp2 = head.next;
+        while(tmp2!=tail){
+            tmp1 = tmp1.next;
+            tmp2 = tmp2.next;
+        }
+
+        //reaching here means tmp1==index of(tail-1) , tmp2 = tail
+        E lastData = tmp2.data;
+        tmp1.next=null;
+        tail=tmp1;
+        currentSize--;
+        return lastData;
+    }
+
+    /**
+     * remove object from linkedlist,O(n) operation as it has to search first
+     * @param obj
+     * @return E
+     */
+    public E remove(E obj){
+        if(head==null){
+            return null;
+        }
+
+        Node<E> tmp1 = null;
+        Node<E> tmp2 = head;
+        while (tmp1!=tail){
+            //we are still looping through the linkedlist
+            if(((Comparable<E>) obj).compareTo(tmp2.data) == 0){
+                //we found a match
+                if(tmp2==head){
+                    return removeFirst();
+                }
+                if(tmp2==tail){
+                    return removeLast();
+                }
+                E foundMatch = tmp2.data;
+                tmp1.next = tmp2.next;
+                currentSize--;
+                return foundMatch;
+            }
+            tmp1=tmp2;
+            tmp2=tmp2.next;
+        }
+
+        //if we reached here, this will mean we couldn't find a match
+        return null;
+    }
+
+    /**
+     * return the first element on linkedlist, without removing,O(1) operation
+     * @return
+     */
+    public E peekFirst(){
+        if(head==null){
+            return null;
+        }
+        return head.data;
+    }
+
+    /**
+     * return the last element on linkedlist,without removing , O(1) operation
+     * @return
+     */
+    public E peekLast(){
+        if(tail==null){
+            return null;
+        }
+        return tail.data;
+    }
+
+    /**
+     * get current size of linkedlist
+     * @return
+     */
+    public int getCurrentSize(){
+        return currentSize;
+    }
 }
---
+++ src/com/company/test/LinkedListTest.java
@@ -0,0 +1,178 @@
+package com.company.test;
+
+import com.company.ds.LinkedList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addFirstOnEmptyLinkedList() {
+        LinkedList l = new LinkedList();
+        l.addFirst("test");
+        assertEquals(l.getCurrentSize(),1);
+        assertEquals(l.peekFirst(),"test");
+    }
+
+    @Test
+    void addFirstOnSingleElementLinkedList() {
+        LinkedList l = new LinkedList();
+        l.addFirst("test");
+        l.addFirst("test2");
+        assertEquals(l.getCurrentSize(),2);
+        assertEquals(l.peekFirst(),"test2");
+    }
+
+    @Test
+    void addFirstOnManyElementsLinkedList() {
+        LinkedList l = new LinkedList();
+        l.addFirst("test");
+        l.addFirst("test2");
+        l.addFirst("test3");
+        assertEquals(l.getCurrentSize(),3);
+        assertEquals(l.peekFirst(),"test3");
+    }
+
+    @Test
+    void addLastOnEmptyLinkedList() {
+        LinkedList l = new LinkedList();
+        l.addLast("test");
+        assertEquals(l.getCurrentSize(),1);
+        assertEquals(l.peekLast(),"test");
+    }
+
+    @Test
+    void addLastOnSingleLinkedList() {
+        LinkedList l = new LinkedList();
+        l.addLast("test");
+        l.addLast("test2");
+        assertEquals(l.getCurrentSize(),2);
+        assertEquals(l.peekLast(),"test2");
+    }
+
+    @Test
+    void addLastOnManyLinkedList() {
+        LinkedList l = new LinkedList();
+        l.addLast("test");
+        l.addLast("test2");
+        l.addLast("test3");
+        assertEquals(l.getCurrentSize(),3);
+        assertEquals(l.peekLast(),"test3");
+    }
+
+    @Test
+    void removeFirstOnEmptyLinkedList() {
+        LinkedList l = new LinkedList();
+        assertNull(l.removeFirst());
+    }
+
+    @Test
+    void removeFirstOnSingleLinkedList() {
+        LinkedList l = new LinkedList();
+        l.addFirst("test");
+        assertEquals(l.removeFirst(),"test");
+        assertEquals(l.getCurrentSize(),0);
+    }
+
+    @Test
+    void removeFirstOnManyLinkedList() {
+        LinkedList l = new LinkedList();
+        l.addFirst("test");
+        l.addFirst("test2");
+        assertEquals(l.removeFirst(),"test2");
+        assertEquals(l.getCurrentSize(),1);
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeLast() {
+        LinkedList l = new LinkedList();
+        for (int i=0;i<100;i++){
+            l.addFirst(Math.random());
+        }
+        l.removeLast();
+        assertEquals(l.getCurrentSize(),99);
+    }
+
+    @Test
+    void removeObjectWhichExistOnFirst() {
+        LinkedList l = new LinkedList();
+        l.addLast("test");
+        l.addLast("test2");
+        l.addLast("test3");
+        assertEquals(l.remove("test"),"test");
+    }
+
+    @Test
+    void removeObjectWhichExistOnLast() {
+        LinkedList l = new LinkedList();
+        l.addLast("test");
+        l.addLast("test2");
+        l.addLast("test3");
+        assertEquals(l.remove("test3"),"test3");
+    }
+
+    @Test
+    void removeObjectWhichExistOnMiddle() {
+        LinkedList l = new LinkedList();
+        l.addLast("test");
+        l.addLast("test2");
+        l.addLast("test3");
+        assertEquals(l.remove("test2"),"test2");
+    }
+
+    @Test
+    void removeObjectWithNoMatchesInLinkedlist() {
+        LinkedList l = new LinkedList();
+        l.addLast("test");
+        l.addLast("test2");
+        l.addLast("test3");
+        assertNull(l.remove("test4"));
+    }
+
+    @Test
+    void removeObjectFromEmptyLinkedlist() {
+        LinkedList l = new LinkedList();
+        assertNull(l.remove("test4"));
+    }
+
+    @Test
+    void peekFirst() {
+        LinkedList l = new LinkedList();
+        l.addLast("test");
+        l.addLast("test2");
+        l.addLast("test3");
+        assertEquals(l.peekFirst(),"test");
+    }
+
+    @Test
+    void peekFirstOnEmptyLinkedList() {
+        LinkedList l = new LinkedList();
+        assertNull(l.peekFirst());
+    }
+
+
+    @Test
+    void peekLast() {
+        LinkedList l = new LinkedList();
+        l.addLast("test");
+        l.addLast("test2");
+        l.addLast("test3");
+        assertEquals(l.peekLast(),"test3");
+    }
+
+    @Test
+    void peekLastOnEmptyLinkedList() {
+        LinkedList l = new LinkedList();
+        assertNull(l.peekLast());
+    }
+}
