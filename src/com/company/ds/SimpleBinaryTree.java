da445303c58efc18651ed74769a6f1c02c029f81
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-02-22 17:06:43 +0200

    write remove method for simple binary tree

--- src/com/company/ds/SimpleBinaryTree.java
+++ src/com/company/ds/SimpleBinaryTree.java
@@ -76,74 +76,122 @@ public class SimpleBinaryTree<E extends Comparable<E>> {
         }
     }
 
-//    public boolean remove(E obj){
-//        if(root==null){
-//            //tree is not initailiazed yet
-//            return false;
-//        }
-//
-//        Node<E> pointerToCurrent = root;
-//        Node<E> pointerToParent = null;
-//
-//        while (pointerToCurrent!=null){
-//            if(pointerToCurrent.data.compareTo(obj) == 0){
-//                //we are ready to remove
-//                    //node found is leaf element
-//                    if(pointerToCurrent.left==null && pointerToCurrent.right==null){
-//                        if(pointerToCurrent==root){
-//                            root=null;
-//                            elementCounter--;
-//                            return true;
-//                        }
-//                        if(pointerToParent.left!=null && obj.compareTo(pointerToParent.left.data) ==0){
-//                            pointerToParent.left=null;
-//                            elementCounter--;
-//                            return true;
-//                        }
-//                        else if(pointerToParent.right!=null && obj.compareTo(pointerToParent.right.data) ==0){
-//                            pointerToParent.right=null;
-//                            elementCounter--;
-//                            return true;
-//                        }
-//                    }
-//
-//                    //node found has only one child
-//                    if(pointerToCurrent.left==null && pointerToCurrent.right!=null){
-//                        if(pointerToCurrent==root){
-//                            root=pointerToCurrent.
-//                        }
-//                       pointerToParent.left = pointerToCurrent.left;
-//                       elementCounter--;
-//                       return true;
-//                    }else if(pointerToCurrent.right==null && pointerToCurrent.left!=null){
-//                        pointerToParent.right = pointerToCurrent.right;
-//                        elementCounter--;
-//                        return true;
-//                    }
-//
-//                    //node found has both left and right
-//                    if(pointerToCurrent.left!=null && pointerToCurrent.right!=null){
-//                        Node<E> successor = getSuccessor(pointerToCurrent);
-//                        E tmpData = pointerToCurrent.data;
-//                        E succTmpData = successor.data;
-//
-//                        pointerToCurrent.data = succTmpData;
-//                        successor.data = tmpData;
-//                        remove(successor.data);
-//                        elementCounter--;
-//                        return true;
-//                    }
-//
-//            }
-//
-//            pointerToParent = pointerToCurrent;
-//            pointerToCurrent = (obj.compareTo(pointerToCurrent.data)>0)
-//                    ?pointerToCurrent.right
-//                    :pointerToCurrent.left;
-//        }
-//
-//        return false;
-//    }
+    public boolean remove(E obj){
+        return remove(obj,root);
+    }
+
+    private boolean remove(E obj,Node<E> node){
+        if(node==null){
+            return false;
+        }
+
+        if(obj.compareTo(node.data)>0){
+            if(node.right !=null){
+                if(obj.compareTo(node.right.data) == 0){
+                    //apply remove logic
+                    return applyRemoveRightChildOf(node);
+                }
+                return remove(obj,node.right);
+            }
+        }else {
+            if(node.left != null) {
+                if (obj.compareTo(node.left.data) == 0) {
+                    //apply remove logic
+                    return applyRemoveLeftChildOf(node);
+                }
+                return remove(obj,node.left);
+            }
+        }
+
+        return false;
+    }
+
+    private boolean applyRemoveLeftChildOf(Node<E> parentNode){
+        if(!validateLeftChildNodeIsLegalToBeRemoved(parentNode)){
+            return false;
+        }
+
+        Node<E> childNodeToBeRemoved = parentNode.left;
+
+        if(isNodeIsLeaf(childNodeToBeRemoved)){
+            parentNode.left=null;
+        }
+
+        if(isNodeHasOnlyLeftChild(childNodeToBeRemoved)){
+            parentNode.left = childNodeToBeRemoved.left;
+            return true;
+        }else if(isNodeHasOnlyRightChild(childNodeToBeRemoved)){
+            parentNode.left = childNodeToBeRemoved.right;
+            return true;
+        }
+
+        if(isNodeHasBothLeftAndRightChild(childNodeToBeRemoved)){
+            Node<E> successor = getSuccessor(childNodeToBeRemoved);
+            swapTwoNodes(childNodeToBeRemoved,successor);
+            return remove(successor.data,parentNode);
+        }
+
+        return false;
+    }
+
+    private boolean applyRemoveRightChildOf(Node<E> parentNode){
+        if(!validateRightChildNodeIsLegalToBeRemoved(parentNode)){
+            return false;
+        }
+
+        Node<E> childNodeToBeRemoved = parentNode.right;
+
+        if(isNodeIsLeaf(childNodeToBeRemoved)){
+            parentNode.right=null;
+            return true ;
+        }
+
+        if(isNodeHasOnlyLeftChild(childNodeToBeRemoved)){
+            parentNode.right = childNodeToBeRemoved.left;
+            return true;
+        }else if(isNodeHasOnlyRightChild(childNodeToBeRemoved)){
+            parentNode.right = childNodeToBeRemoved.right;
+            return true;
+        }
+
+        if(isNodeHasBothLeftAndRightChild(childNodeToBeRemoved)){
+            Node<E> successor = getSuccessor(childNodeToBeRemoved);
+            swapTwoNodes(childNodeToBeRemoved,successor);
+            return remove(successor.data,parentNode);
+        }
+
+        return false;
+    }
+
+    private boolean validateLeftChildNodeIsLegalToBeRemoved(Node<E> parentNode){
+        return (parentNode != null && parentNode.left !=null);
+    }
+
+    private boolean validateRightChildNodeIsLegalToBeRemoved(Node<E> parentNode){
+        return (parentNode != null && parentNode.right !=null);
+    }
+
+    private boolean isNodeIsLeaf(Node<E> node){
+        return (node.left == null && node.right == null);
+    }
+
+    private boolean isNodeHasOnlyLeftChild(Node<E> node){
+        return (node.left != null && node.right == null);
+    }
+
+    private boolean isNodeHasOnlyRightChild(Node<E> node){
+        return (node.left == null && node.right != null);
+    }
+
+    private boolean isNodeHasBothLeftAndRightChild(Node<E> node){
+        return (node.left != null && node.right != null);
+    }
+
+    private void swapTwoNodes(Node<E> node1,Node<E> node2){
+        E tmpData = node1.data;
+        node1.data = node2.data;
+        node2.data = tmpData;
+    }
 
     private Node<E> getSuccessor(Node<E> node){
         if(node==null){
