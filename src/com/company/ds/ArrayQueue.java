77bd135a1b13b42678b52ffaf66a4d07c1341084
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-03-22 03:18:20 +0200

    fix array queue pointers logic

--- src/com/company/ds/ArrayQueue.java
+++ src/com/company/ds/ArrayQueue.java
@@ -2,41 +2,55 @@ package com.company.ds;
 
 public class ArrayQueue<E> {
     private Object[] array;
-    private int read=0,write=0;
+    private int read=-1,write=-1;
     private int count = 0;
 
     public ArrayQueue(int size){
-        if(size<=1){
+        if(size<1){
             throw new IllegalArgumentException();
         }
         array = new Object[size];
     }
 
     public void enqueue(E elem) throws Exception{
-        if(read == (write+1)%array.length){
+        if(isFull()){
             Object[] scaledArray = new Object[count*2];
             System.arraycopy(array,0,scaledArray,0,count);
             this.array = scaledArray;
         }
 
-        array[write] = elem;
         write = (write+1) % array.length;
+        array[write] = elem;
         count++;
     }
 
-    public E dequeue() throws Exception{
-        if(read == write){
-            throw new Exception("Queue is empty");
+    public E dequeue(){
+        if(isEmpty()){
+            throw new RuntimeException("Queue is empty");
         }
 
         E data = (E) array[read];
+
+        if(read == write){
+            read = write = -1;
+            count--;
+            return data;
+        }
+
         read=(read+1) % array.length;
         count--;
         return data;
     }
 
+    public boolean isEmpty(){
+        return read == -1 && write == -1;
+    }
+
+    public  boolean isFull(){
+        return read == (write+1) % array.length;
+    }
+
     public int getCount(){
         return count;
     }
-
 }
--- src/com/company/test/ArrayQueueTest.java
+++ src/com/company/test/ArrayQueueTest.java
@@ -35,6 +35,6 @@ public class ArrayQueueTest {
 
         }
 
-        assertEquals(data,11);
+        assertEquals(11,data);
     }
 }
