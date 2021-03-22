bdb3b8f96086f6d71f80c1c1a91564f9527a0a91
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-03-22 03:25:06 +0200

    write test on array queue

--- src/com/company/test/ArrayQueueTest.java
+++ src/com/company/test/ArrayQueueTest.java
@@ -3,8 +3,7 @@ package com.company.test;
 import com.company.ds.ArrayQueue;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayQueueTest {
     @Test
@@ -24,5 +23,45 @@ public class ArrayQueueTest {
         q.enqueue(22);
         q.enqueue(33);
         assertEquals(11,q.dequeue());
+        assertEquals(22,q.dequeue());
+        assertEquals(33,q.dequeue());
+    }
+
+    @Test
+    public void isEmpty(){
+        ArrayQueue<Integer> q = new ArrayQueue<Integer>(10);
+        assertTrue(q.isEmpty());
+    }
+
+    @Test
+    public void isNotEmpty(){
+        ArrayQueue<Integer> q = new ArrayQueue<Integer>(10);
+        q.enqueue(2);
+        assertFalse(q.isEmpty());
+    }
+
+    @Test
+    public void isEmptyAfterOperation(){
+        ArrayQueue<Integer> q = new ArrayQueue<Integer>(10);
+        q.enqueue(22);
+        q.dequeue();
+        assertTrue(q.isEmpty());
+    }
+
+    @Test
+    public void isFull(){
+        ArrayQueue<Integer> q = new ArrayQueue<Integer>(2);
+        q.enqueue(1);
+        q.enqueue(2);
+        assertTrue(q.isFull());
+    }
+
+    @Test
+    public void isNotFull(){
+        ArrayQueue<Integer> q = new ArrayQueue<Integer>(2);
+        q.enqueue(1);
+        q.enqueue(2);
+        q.dequeue();
+        assertFalse(q.isFull());
     }
 }
